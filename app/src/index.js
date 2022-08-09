5c7b8469a9b42ca0fec6704efea9eef11d9c9a0e
Alan Flores <alanif@hotmail.com>
2022-08-08 21:19:57 -0500

    add modal

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,17 @@
 import post from "./components/post.js"
 
-const main = document.querySelector(".main")
+const main = document.querySelector(".main");
+main.insertAdjacentElement("afterbegin",post());
 
-main.insertAdjacentElement("afterbegin",post())
+const infoModal = document.querySelector(".infoModal");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".modal__close");
+
+infoModal.addEventListener("click", ()=>{
+  modal.style.display = "flex";
+  modal.showModal();
+})
+closeModal.addEventListener("click",()=>{
+  modal.close();
+  modal.style.display = "none";
+})
